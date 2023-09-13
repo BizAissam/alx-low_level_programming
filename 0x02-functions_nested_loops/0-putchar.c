1aa2296e4a8e641898e71da2417aaa52c28a938a
BizAissam <bizri.aissam@gmail.com>
2023-09-13 12:57:32 -0700

    functions and nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-
 /**
-* main - Prints _putchar
-*
-* Return: Always 0 (Success)
-*/
+ * main - print _putchar
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-  putchar('_');
-  putchar('p');
-  putchar('u');
-  putchar('c');
-  putchar('h');
-  putchar('a');
-  putchar('r');
-  putchar('\n');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
